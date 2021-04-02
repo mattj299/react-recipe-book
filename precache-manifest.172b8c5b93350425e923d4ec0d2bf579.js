d3bf14fcb8993b7994e62c1902fd374df3bdf220
mattj299 <mateonas586@gmail.com>
2021-04-01 17:20:36 -0700

    Updates

---
+++ precache-manifest.172b8c5b93350425e923d4ec0d2bf579.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f915d9829994821e56722334724cd58",
+    "url": "/recipe-book/index.html"
+  },
+  {
+    "revision": "ed325f5be3d14688d734",
+    "url": "/recipe-book/static/css/main.a02b636b.chunk.css"
+  },
+  {
+    "revision": "3fdf9502182162c21f79",
+    "url": "/recipe-book/static/js/2.bce34500.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipe-book/static/js/2.bce34500.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed325f5be3d14688d734",
+    "url": "/recipe-book/static/js/main.04e5637b.chunk.js"
+  },
+  {
+    "revision": "bc080b6ba01eef4b319d",
+    "url": "/recipe-book/static/js/runtime-main.52f5807f.js"
+  }
+]);
